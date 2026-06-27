45ed4652df192b3c40fe076bb001981dcf09980e
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-06-27 23:54:17 +0000

    created a script that allows me to add data to my mongodb database using the CLI

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const {Schema, Model} = mongoose;
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://root:${password}@sandbox.pjn5u.mongodb.net/phonebook?appName=Sandbox`;
+
+// connect to DB 
+mongoose.connect(url)
+.then(()=>{
+  console.log("db connected successful");
+
+  // create schema 
+  const contactSchema = new mongoose.Schema({name:String, number:String});
+  
+  // create model 
+  const Contact = mongoose.model("contact", contactSchema);
+
+  // create new contact 
+  let contact = new Contact({name:"Boakai", number:"0885332142"});
+  contact.save().then(result=>{
+    console.log(`added ${result.name} ${result.number}`);
+    mongoose.connection.close();
+  })
+
+  
+
+ 
+
+
+
+})
+.catch(error=> console.log("error", error));
+
+
+
