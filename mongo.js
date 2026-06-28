9a17ecad58ad8c8f1ca684fba4d75c0049c287e5
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-06-28 00:04:38 +0000

    update script to query all contacts from db

--- mongo.js
+++ mongo.js
@@ -10,7 +10,9 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
+
 const url = `mongodb+srv://root:${password}@sandbox.pjn5u.mongodb.net/phonebook?appName=Sandbox`;
+
 
 // connect to DB 
 mongoose.connect(url)
@@ -24,11 +26,20 @@ mongoose.connect(url)
   const Contact = mongoose.model("contact", contactSchema);
 
   // create new contact 
-  let contact = new Contact({name:"Boakai", number:"0885332142"});
-  contact.save().then(result=>{
-    console.log(`added ${result.name} ${result.number}`);
-    mongoose.connection.close();
-  })
+  if(name && number){
+    // ADD TO DB 
+    let contact = new Contact({name:name, number:number});
+
+    contact.save().then(result=>{
+      console.log(`added ${result.name} ${result.number}`);
+      mongoose.connection.close();
+    })
+  }else{
+    Contact.find({}).then(result=>{
+      console.log(result);
+      mongoose.connection.close();
+    })
+  }
 
   
 
